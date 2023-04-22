e2c0abd75f9dc98fac1af1009f95bbcd4ce63c73
webdev-ashishk <webdev.ashishk@gmail.com>
2023-04-22 12:30:38 +0530

    episode 02 completed

--- Ep-02/app.js
+++ Ep-02/app.js
@@ -1,4 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom";
-const heading = React.createElement("h1", { id: "h1" }, "React is working....");
+import React from "react"; //Here importarting react from node_module
+import ReactDOM from "react-dom"; //Here importarting react from node_module
+const heading = React.createElement("h1", { id: "h1" }, "Namate react..");
 ReactDOM.render(heading, document.getElementById("root"));
