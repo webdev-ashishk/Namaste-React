8ec65f02c01db5f4a3df9d4e19f0d6eb18aa8c28
webdev-ashishk <webdev.ashishk@gmail.com>
2023-04-22 07:41:10 +0530

    importing react from npm to my project

---
+++ Ep-02/app.js
@@ -0,0 +1,4 @@
+import React from "react";
+import ReactDOM from "react-dom";
+const heading = React.createElement("h1", { id: "h1" }, "React is working....");
+ReactDOM.render(heading, document.getElementById("root"));
