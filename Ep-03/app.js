f9ca967271dd9ed5aec1c8c78a9744f550b093ba
webdev-ashishk <webdev.ashishk@gmail.com>
2023-04-23 08:34:53 +0530

    episode 03 part 3 Home-wrork completed

--- Ep-03/app.js
+++ Ep-03/app.js
@@ -5,9 +5,9 @@ const heading = React.createElement("h1", { id: "h1" }, "Namate react..");
 //react.createElement return a object
 //render method convert the object into hmtl and display to screen!
 console.log(heading);
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root1 = ReactDOM.createRoot(document.getElementById("root1"));
 // ReactDOM.render(heading, document.getElementById("root"));
-root.render(heading);
+root1.render(heading);
 //creating react.createElement is
 //When we create Nested Structure then this code become very complex then facebook developer develop the JSX(javascript XML)
 
@@ -23,7 +23,7 @@ TS:18:12sec JSX  */
 
 const jsxHeading = <h1 id="h1">Namaste React writen using JSX</h1>;
 /* 
-//NOTE: 
+NOTE: 
 1] javascript engine can not understand jsxheading
 2] jsxheading is not javascript
 Q1: How browser render the peice of code!
@@ -43,13 +43,30 @@ Babel is a free and open-source JavaScript transcompiler that is mainly used to 
 
 */
 console.log(jsxHeading);
-root.render(jsxHeading);
+root1.render(jsxHeading);
 //jsxHeading is react Element--->React element is Object--->JSX is syntex
 
 //Q2: How to write multiple line of JSX code
-const element =
-  ((<h2>Heading Two Writen By JSX</h2>),
-  (<h3>Heading three Writen by JSX</h3>));
+//A:
 
 //Practice all JSX Syntex after that came back here !
+//used file's practiceJSX.js now
+//follow this doc link  https://react.dev/learn/writing-markup-with-jsx#1-return-a-single-root-element
 // <---------------------Ep-03 part 03 end----------------->
+
+const element = (
+  <>
+    <h1>heading-one</h1>
+    <h2>Heading-two</h2>
+    <h3>Heading-three</h3>
+    <h4>Heading-four</h4>
+    <p>This is first para</p>
+    <p>This is second Para</p>
+    <img
+      src="https://avatars.githubusercontent.com/u/127021921?v=4"
+      alt="github-photo"
+    />
+  </>
+);
+const root2 = ReactDOM.createRoot(document.getElementById("root2"));
+root2.render(element);
