841f46efe60593ea55fe1378b4968295f37513b5
webdev-ashishk <webdev.ashishk@gmail.com>
2023-12-15 21:52:17 +0530

    ready for making card

--- Ep-07/src/components/Body.jsx
+++ Ep-07/src/components/Body.jsx
@@ -1,24 +1,32 @@
 import { useEffect, useState } from "react";
+import RestaurantCard from "./RestaurantCard";
 
 const Body = () => {
-  const [restaurants, setRestaurants] = useState([]);
+  const [allRestaurants, setAllRestaurants] = useState([]);
   // const [restaurantList, setRestaurantList] = useState([]);
   const API_URL =
     "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.5725777&lng=77.3205965&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
   async function apiCall() {
     const data = await fetch(API_URL);
     const json = await data.json();
-    setRestaurants(json);
+    setAllRestaurants(
+      json.data.cards[5].card.card.gridElements.infoWithStyle.restaurants
+    );
   }
 
   // console.log(
   //   restaurants.data.cards[5].card.card.gridElements.infoWithStyle.restaurants
   // );
-  console.log(restaurants);
+  // console.log(allRestaurants);
 
   useEffect(() => {
     apiCall();
   }, []);
-  return <div>body and Home page Rendered *</div>;
+  return (
+    <div>
+      body and Home page Rendered *
+      <RestaurantCard data={allRestaurants} />
+    </div>
+  );
 };
 export default Body;
---
+++ Ep-07/src/components/RestaurantCard.jsx
@@ -0,0 +1,9 @@
+const RestaurantCard = (props) => {
+  console.log(props);
+  return (
+    <div>
+      all restaurants #{/* {props.map((r)=>(<div key={index}></div>))} */}
+    </div>
+  );
+};
+export default RestaurantCard;
