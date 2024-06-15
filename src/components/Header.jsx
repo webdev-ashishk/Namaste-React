9962417f7d621beba2b1c37a0ed9dacbfb4581ee
webdev-ashishk <webdev.ashishk.com>
2024-06-15 07:53:54 +0530

    ready to optimize a performate app

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,22 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
   return (
     <div>
       <p>Header.jsx rendered!</p>
+      <nav>
+        <ul>
+          <li>
+            <Link to={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link to={"/about"}>about</Link>
+          </li>
+          <li>
+            <Link to={"/contact"}>contact</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
